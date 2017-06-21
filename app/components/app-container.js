b6fa5604d899389d8a4f04f8a81dd862b3f0d6d8
alvaro <alvarojosepl@gmail.com>
2017-06-21 09:23:26 +0200

    minimum setup for a react native application

---
+++ app/components/app-container.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators } from '../actions';
+
+import { Actions, Router, Scene } from 'react-native-router-flux';
+
+import dummy from './dummy-view'
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const scenes = Actions.create(
+    <Scene key="root" hideNavBar={true} tabs={false} >
+        <Scene key="tabbar" tabs={false} hideNavBar={true} initial>
+            <Scene key='test' component={dummy} title="Test" initial/>
+        </Scene>
+     </Scene>
+     
+);
+
+class AppContainer extends Component {
+render() {
+        return <Router  {...this.props} scenes={scenes}/>
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators(ActionCreators, dispatch);
+}
+
+export default connect(() => {return {}}, mapDispatchToProps)(AppContainer);
